c9c239b2668a0a1783de721c288be0a461ad3809
Lul-Abdifan <tasanbannu@gmail.com>
2024-12-04 15:30:05 +0300

    feat(web): Add Button Atom

---
+++ components/Atoms/Button.vue
@@ -0,0 +1,50 @@
+<template>
+  <button
+    :class="[
+      'w-full', 
+      `h-${size}`, 
+      `rounded-${rounded}`, 
+      bgColor, 
+      textColor, 
+      'font-semibold', 
+      `hover:${hoverBgColor}`, 
+      'transition-all'
+    ]"
+    @click="handleClick"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    size: {
+      type: String,
+      default: "11", 
+    },
+    rounded: {
+      type: String,
+      default: "full", 
+    },
+    bgColor: {
+      type: String,
+      default: "bg-primary", 
+    },
+    hoverBgColor: {
+      type: String,
+      default: "bg-secondary", 
+    },
+    textColor: {
+      type: String,
+      default: "text-white", 
+    },
+  },
+  methods: {
+    handleClick() {
+      this.$emit("click"); 
+    },
+  },
+};
+</script>
