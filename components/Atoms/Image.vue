028c84ea7709f281ad82f3e3c2cd5c91dd216677
Lul-Abdifan <tasanbannu@gmail.com>
2024-12-04 15:33:57 +0300

    feat(web): Add Reuseable Components

---
+++ components/Atoms/Image.vue
@@ -0,0 +1,23 @@
+<template>
+    <img
+      :src="src"
+      :alt="alt"
+      :width="width"
+      :height="height"
+      :class="customClass"
+      loading="lazy"
+    />
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      src: { type: String, required: true },
+      alt: { type: String, default: "" },
+      width: { type: [String, Number], default: "auto" },
+      height: { type: [String, Number], default: "auto" },
+      customClass: { type: String, default: "" },
+    },
+  };
+  </script>
+  
